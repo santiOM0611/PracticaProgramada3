594fef19ad18180c1df7689b9d8163045ce55f7d
Santiago Orozco <sorozco80826@ufide.ac.cr>
2026-04-03 17:13:49 -0600

    MVC
    MVC y paquetes NuGet.

---
+++ TiquetesApp/TiquetesApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TiquetesApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
